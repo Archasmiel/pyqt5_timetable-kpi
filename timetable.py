5c1d1d1e06afa05119a7fe70f6af6554b30c1b9b
Archasmiel <s.remez-me23@lll.kpi.ua>
2022-04-04 16:22:30 +0300

    Upload 04.04.2022 - added ignoring subjects - minor changes to dark theme - current lesson highlighting

--- timetable.py
+++ timetable.py
@@ -12,18 +12,19 @@ def create_table_start(table_id):
            f'cellpadding="10" rules="all" align="center" border="1">'
 
 
-def remove_links(html_text):
+def remove_links(html_text, colors):
     text = ''
+    ignore, ignored = read_ignore()
 
     re1, re2 = '<a[^\>]+">', '</a>'
-    for i in html_text.split('\n'):
-        color1, color2 = '#5B7DB1', '#61A4BC'
+    color1, color2 = colors[0], colors[1]
+    days = '<td></td><td>Понеділок</td><td>Вівторок</td>' \
+           '<td>Середа</td><td>Четвер</td><td>П\'ятниця</td><td>Субота</td>'
 
-        days = '<td></td><td>Понеділок</td><td>Вівторок</td>' \
-               '<td>Середа</td><td>Четвер</td><td>П\'ятниця</td><td>Субота</td>'
+    for i in html_text.split('\n'):
 
         if days in i:
-            i = re.sub(days, f'<td></td>'
+            i = re.sub(days, f'<td class="availa"></td>'
                              f'<td><font color={color1}>Понеділок</font></td>'
                              f'<td><font color={color1}>Вівторок</font></td>'
                              f'<td><font color={color1}>Середа</font></td>'
@@ -50,6 +51,19 @@ def remove_links(html_text):
         if '<a class="plainLink" href=' in i:
             i = re.sub(re1, '', i)
             i = re.sub(re2, '', i)
+
+        if ignore:
+            for j in ignored:
+                if len(j) == 1:
+
+                    ind = f'<span class="disLabel">{j[0]}<\/span><br\/>[^\>]+<br\/>[^\>]+ <font color={color2}><br>[^\>]+<\/font>'
+                    if j[0] in i:
+                        i = re.sub(ind, '', i)
+
+        if 'class="day_backlight"' in i:
+            i = re.sub('class="day_backlight"', 'style="color:#000000" bgcolor=#ace2de', i)
+        if 'class="current_pair"' in i:
+            i = re.sub('class="current_pair"', 'style="color:#000000" bgcolor=#b0d751', i)
 
         text += i + '\n'
 
@@ -81,14 +95,30 @@ def get_group(link: str, group: str):
             return i[22:-1]
     return 'no_group'
 
+
+def read_ignore():
+    with open('data/ignore.txt', 'r', encoding='utf-8') as f:
+        data = f.read().split('\n')
+        ignore = True if data[0] == "yes" else False
+        content = []
+        if len(data) > 1:
+            for i in data:
+                if not (i == 'yes' or i == 'no'):
+                    content.append(i.split('<br>'))
+
+    print(content)
+    return ignore, content
+
+
 def get_dark_theme():
     white = QtGui.QColor(255, 255, 255)
     red = QtGui.QColor(255, 0, 0)
+    grey = QtGui.QColor(40, 40, 40)
     black = QtGui.QColor(0, 0, 0)
     palette = QtGui.QPalette()
-    palette.setColor(QtGui.QPalette.Window, black)
+    palette.setColor(QtGui.QPalette.Window, grey)
     palette.setColor(QtGui.QPalette.WindowText, white)
-    palette.setColor(QtGui.QPalette.Base, black)
+    palette.setColor(QtGui.QPalette.Base, grey)
     palette.setColor(QtGui.QPalette.AlternateBase, QtGui.QColor(53, 53, 53))
     palette.setColor(QtGui.QPalette.ToolTipBase, white)
     palette.setColor(QtGui.QPalette.ToolTipText, white)
@@ -123,6 +153,12 @@ def get_light_theme():
     return palette
 
 
+def load_colors():
+    with open('data/colors.txt', 'r') as f:
+        data = f.read().split('\n')
+    return data[0], data[1], 1 if data[2] == 'dark' else 0
+
+
 table_starts = [create_table_start("First"), create_table_start("Second")]
 
 
@@ -132,15 +168,17 @@ class Ui(QtWidgets.QMainWindow):
         super(Ui, self).__init__()
         uic.loadUi(UIfile, self)
 
+        self.color1, self.color2, self.theme = load_colors()
+
         self.tables = [self.textBrowser, self.textBrowser_2]
         self.themes = (
             (get_light_theme, QtGui.QColor(0, 0, 0)),
             (get_dark_theme, QtGui.QColor(255, 255, 255))
         )
-        self.theme = 1
 
         self.setWindowTitle('КПИ расписание')
         self.setWindowIcon(QtGui.QIcon('ui/icon.png'))
+        self.set_theme()
         self.pushButton.clicked.connect(self.search_group)
         self.pushButton_2.clicked.connect(self.change_theme)
         self.show()
@@ -150,9 +188,7 @@ class Ui(QtWidgets.QMainWindow):
         self.tables[i].show()
         self.tables[i].raise_()
 
-    def change_theme(self):
-        # 0 - light, 1 - dark
-        self.theme = 1 if self.theme == 0 else 0
+    def set_theme(self):
         self.setPalette(self.themes[self.theme][0]())
         self.textBrowser.setPalette(self.themes[self.theme][0]())
         self.textBrowser_2.setPalette(self.themes[self.theme][0]())
@@ -161,6 +197,11 @@ class Ui(QtWidgets.QMainWindow):
         palette.setColor(QtGui.QPalette.WindowText, self.themes[self.theme][1])
         self.statusBar.setPalette(palette)
         self.pushButton_2.setText('L' if self.theme == 0 else 'D')
+
+    def change_theme(self):
+        # 0 - light, 1 - dark
+        self.theme = 1 if self.theme == 0 else 0
+        self.set_theme()
 
     def search_group(self):
         if self.sender().text() == self.pushButton.text():
@@ -172,7 +213,7 @@ class Ui(QtWidgets.QMainWindow):
                 return
             self.statusBar.showMessage(f'Найдено {self.lineEdit.text()}: {group_link}.')
 
-            html_text = remove_links(req.get(group_link).text)
+            html_text = remove_links(req.get(group_link).text, (self.color1, self.color2))
             self.show_table(html_text, 0)
             self.show_table(html_text, 1)
 
